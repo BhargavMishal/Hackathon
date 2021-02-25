535c9b0d70a401e7320f1a793cc140ab137b9afd
Bhargav <bhargavmishal1111@gmail.com>
2021-02-25 22:16:56 +0530

    Final

---
+++ Backend/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('ROOT_URL', 'http://localhost/INP_PROJECT/Swadeshi/Backend/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'ecommerce3');
+?>
